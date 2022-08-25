7aa72f54f76ce4e60bff2e974b269c0c6a14e044
Emanuel Komínek <drake127@planescape.cz>
2022-08-25 10:29:15 +0200

    Initial mod commit.

---
+++ randomrolls/tools/sources/main.cpp
@@ -0,0 +1,33 @@
+﻿/**
+ * @author Emanuel Komínek <drake127@planescape.cz>
+ * @license Public Domain
+ * @file
+ */
+
+#include <intrin.h>
+
+bool rdrand_supported()
+{
+	int registers[4];
+	__cpuid(registers, 1);
+	return registers[2] & (1 << 30);
+}
+
+bool rdrand_sensible()
+{
+	for (int i = 0; i < 10; ++i)
+	{
+		unsigned int rand;
+		if (!_rdrand32_step(&rand))
+			break;
+		if (rand != 0xFFFFFFFF && rand != 0x00000000)
+			return true;
+	}
+
+	return false;
+}
+
+int main()
+{
+	return !(rdrand_supported() && rdrand_sensible());
+}
